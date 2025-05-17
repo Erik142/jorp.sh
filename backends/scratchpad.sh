Erik142/jorp.sh

commit ec230e6aafeef4ed57c4f3e2b7512059e7c2b19d

fix(scratchpad): do not assume that tmux is used as multiplexer

backends/scratchpad.sh
@@ -67,10 +67,4 @@ function scratchpad_run_batch() {
 
     eval "$action"
   fi
-
-  if [[ "$TERM_PROGRAM" == "tmux" ]]; then
-    eval tmux "$TMUX_OPTS" switch -t "$session_name"
-  else
-    eval tmux "$TMUX_OPTS" attach-session -t "$session_name"
-  fi
 }

commit 22cf0d8291880697f6e25fa13cd9e66a6cc322a2

fix(scratchpad): use tmux for switching sessions
The project is not in a state where it is multiplexer-agnostic yet.
Use tmux as the default multiplexer of choice for now.

Fixes: ec230e6 (fix(scratchpad): do not assume that tmux is used as multiplexer, 2024-09-27)

backends/scratchpad.sh
@@ -54,6 +54,7 @@ function scratchpad_run_batch() {
 
   if [ "$open_existing_scratchpad" == "y" ]; then
     session_name="$(scratchpad_get_session_name "$scratchpad_name")"
+    tmux_select_item "$session_name"
   fi
 
   if [ -z "$session_name" ]; then